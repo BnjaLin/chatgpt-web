852c3e31bc290743a2d6633d326e18b50c19a88d
Ben <ben@asano.com.cn>
2023-04-26 17:19:25 +0800

    chore: update permission

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -481,7 +481,7 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-            <span>欢迎来到 Asano AI 智能大模型，很高兴为您排忧解难</span>
+              <span>欢迎来到 Asano AI 智能大模型，很高兴为您排忧解难</span>
             </div>
           </template>
           <template v-else>
